491e1661ace26ab2540b5cfb9b6c2f0f40a4b65d
midnightvibe <flaviacolour@gmail.com>
2023-02-13 17:28:56 +0100

    hangman was added

---
+++ Hangman.py
@@ -0,0 +1,47 @@
+# Step 1
+import random
+from hangman_art import logo
+from hangman_art import stages
+from hangman_words import word_list
+
+print(logo)
+
+chosen_word = random.choice(word_list)
+print(f"Pssst, the solution is {chosen_word}")
+
+display = []
+for _ in chosen_word:
+    display += "_"
+
+lives = 6
+end_of_game = False
+previous_guess = []
+while not end_of_game and not lives == 0:
+
+    guess = input("Guess a letter: ").lower()
+
+    if guess == previous_guess:
+        print(f"You have already guessed the letter: {previous_guess}")
+        print(f"{' '.join(display)}")
+    else:
+        word_length = len(chosen_word)
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if guess == letter:
+                display[position] = letter
+
+        if guess not in display:
+            lives -= 1
+            print(f"You lose a live, current lives: {lives}")
+            if lives == 0:
+                end_of_game = True
+                print("You Lose!")
+
+        print(f"{' '.join(display)}")
+        if "_" not in display:
+            end_of_game = True
+            print("You Win!")
+
+    previous_guess = guess
+
+    print(stages[lives])
