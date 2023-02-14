e1923fa3f4b92abfca9a30f1f819628a065797bf
midnightvibe <flaviacolour@gmail.com>
2023-02-14 20:16:24 +0100

    Caeser Cipher was added

---
+++ Caeser_Cipher.py
@@ -0,0 +1,32 @@
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        if letter not in alphabet:
+            end_text += letter
+        else:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+    print(f"Your {direction}d text is: {end_text}")
+
+
+print(logo)
+run_again = True
+while run_again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift %= 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    again = input('Wanna run again answer with "yes" or "no"?: ')
+    if again == "no":
+        run_again = False
