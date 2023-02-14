b36b48774fe2bf23ac45f85b40bfb022b5421c98
midnightvibe <flaviacolour@gmail.com>
2023-02-14 14:20:54 +0100

    bug fix

--- Paint_Area_Calculator.py
+++ Paint_Area_Calculator.py
@@ -1,7 +1,7 @@
 import math
 def paint_calc(height, width, cover):
     area = height * width
-    num_of_cans = math.celi(area / cover)
+    num_of_cans = math.ceil(area / cover)
     print(f"You'll need {num_of_cans} cans of paint.")
 
 
